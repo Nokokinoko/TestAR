9fa557ec9c660cba05bfc77cb4de99aef238c962
津田 翔大 <syota.t@gmail.com>
2020-12-29 03:12:18 +0900

    init setting vuforia

---
+++ Assets/Scripts/MyModelTrackableEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyModelTrackableEventHandler : DefaultTrackableEventHandler
+{
+  protected override void OnTrackingFound()
+  {
+    base.OnTrackingFound();
+
+    if(mTrackableBehaviour.gameObject.GetComponentInChildren<MyModelController>() != null)
+    {
+      mTrackableBehaviour.gameObject.GetComponentInChildren<MyModelController>().StartAnime();
+    }
+  }
+
+  protected override void OnTrackingLost()
+  {
+    base.OnTrackingLost();
+
+    if (mTrackableBehaviour.gameObject.GetComponentInChildren<MyModelController>() != null)
+    {
+      mTrackableBehaviour.gameObject.GetComponentInChildren<MyModelController>().EndAnime();
+    }
+  }
+}
